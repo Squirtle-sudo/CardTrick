a7bda64106ef25da4d3af5d88102ddfc9f4eb111
SebastienPaul <marti310@sheridancollege.ca>
2025-09-17 23:04:08 -0400

    student master code of card trick assignment

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -20,33 +20,43 @@ public class CardTrick {
     
     public static void main(String[] args)
     {
-        Card[] magicHand = new Card[7];
-       
+        Card[] magicHand = new Card[7]; //setting up the magic hand array
+        Card lucky_card = new Card();   //setting up lucky card
+
+        lucky_card.setSuit(lucky_card.SUITS[3]);    //lucky card is 2 of clubs
+        lucky_card.setValue(2);
         
-        for (int i=0; i<magicHand.length; i++)
+        magicHand[0] = lucky_card;  //lucky card is the first card of the hand
+        
+        for (int i=1; i<magicHand.length; i++)  //loop to instantiate the rest of the cards in the lucky hand
         {
             Card c = new Card();
             c.setSuit(Card.SUITS[(int) (Math.random() * 3)]);
             c.setValue((int)(Math.random() * 13));
-            System.out.println(c.getSuit() + c.getValue());
+
             magicHand[i] = c;
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
         
+        
         String card_suit;
+        String[] card_array;
         int card_value;
         int same_card_count = 0;
-        
+            
+            //scanner to check the players card
             Scanner sc = new Scanner(System.in);
             System.out.println("What is the value and the suit of your card?");
             //the user needs to put a space between both values: 13 diamonds or clubs 2
             String userCard = sc.nextLine();
 
-            String[] card_array = userCard.split(" ");
+            //split the card in the number and the string so i can assign it to two different variables
+            card_array = userCard.split(" ");
 
 
-
+            //cant do an if statement so i did a try / catch to see which of the two unknown Strings were infact an int
+            //and then i put them in their corresponding int and string variables to become a new card's value and suite
             try{
                 card_value = Integer.parseInt(card_array[0]);
                 card_suit = card_array[1];
@@ -55,14 +65,18 @@ public class CardTrick {
                 card_suit = card_array[0];
             }
         
+        //setting the new suit and value as the new card
         Card c = new Card();
         c.setSuit(card_suit);
         c.setValue(card_value);
         
+        //checking if the card is the same as any of the other cards in the array
         for( Card magic_hand_card : magicHand){
             if ((magic_hand_card.getSuit().toLowerCase().equals(c.getSuit().toLowerCase())) && (magic_hand_card.getValue() == c.getValue()))
                 same_card_count++;            
         }
+        
+        //checking if theres more than 1 of the same card as the card that was mentionned in the magic hand, or is it one or zero of them
         if (same_card_count > 1){
             System.out.println("There are " + same_card_count +  ",  " + card_value + " of " + card_suit + " in the magic hand.");
         }
